eab6d9d97beffb0f639fc59614abc18a94ab9319
paulsauve55 <paul@psconsultation.ca>
2026-03-03 14:46:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/crm-gnr-farnham/',
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
+  }
 })
